2b73890461141704da848e473df8cd6d4a9ab95e
George Garabedian <gmgarabedian@gmail.com>
2014-09-26 10:25:34 -0700

    first draft home page

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name drinksenderApp
+ * @description
+ * # drinksenderApp
+ *
+ * Main module of the application.
+ */
+angular.module('drinksenderApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.utils',
+    'simpleLogin'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name drinksenderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the drinksenderApp
+ */
+angular.module('drinksenderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
